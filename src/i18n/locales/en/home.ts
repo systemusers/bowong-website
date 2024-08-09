2904de968b5b6796ed3e48d5676572f8fdc9f3f7
菜菜酱 <wwg_c710@163.com>
2024-08-09 15:07:04 +0800

    国际化首页

---
+++ src/i18n/locales/en/home.ts
@@ -0,0 +1,22 @@
+export const home = {
+    head1: 'Build up new AI online marketplace',
+    head2: 'Base on full-automatic director & media AI system，support unlimited content & UGC channel martix',
+
+    /**产品 */
+    product1: '博望出海内容模型',
+    product2: '博望斗彩图片大模型',
+    product3: '博望短视频大模型 ',
+    /**介绍 */
+    product1intro1: 'TikTok/亚马逊内容模型，改变传统出海短视频生产和投放模式，以矩阵+AI生产模式快速布局，快速铺货',
+    product1intro2: '基于多年设计领域经验，建设面相真产品 + 真场景图文大模型，帮助电商客户快速完成内容生产和多渠道投放',
+    product2intro3: 'AI内容+AI渠道能力匹配个性化算法 基于用户的个性化关注，生产内容并全方位触达用户',
+    /*** 技术 */
+    TechnicalName1:'Real-Time breaking news and entertainment Connect with realtime short-video production',
+    TechnicalName2: 'RE-Director short videos and Live cut by full-automatic AI system',
+    TechnicalName3: '还原真实的图片模型',
+    /**技术特点 */
+    Technical1: ' LLM base on online marketplace and all meida platform',
+    Technical2: '多语言支持，语言语音无缝切换',
+    Technical3: '超高速高质量规模化生产',
+  };
+  
---
+++ src/i18n/locales/zh/home.ts
@@ -0,0 +1,21 @@
+export const home = {
+    head1: '基于AI的新电商',
+    head2: '构建AI形态的内容+渠道',
+    /**产品 */
+    product1: '博望出海内容模型',
+    product2: '博望斗彩图片大模型',
+    product3: '博望短视频大模型 ',
+    /**介绍 */
+    product1intro1: 'TikTok/亚马逊内容模型，改变传统出海短视频生产和投放模式，以矩阵+AI生产模式快速布局，快速铺货',
+    product1intro2: '基于多年设计领域经验，建设面相真产品 + 真场景图文大模型，帮助电商客户快速完成内容生产和多渠道投放',
+    product2intro3: 'AI内容+AI渠道能力匹配个性化算法 基于用户的个性化关注，生产内容并全方位触达用户',
+    /*** 技术 */
+    TechnicalName1: '实时热点 X 批量短视频生产',
+    TechnicalName2: '短视频/直播内容 RE-Director',
+    TechnicalName3: '还原真实的图片模型',
+    /**技术特点 */
+    Technical1: '行业/IP/渠道垂直模型',
+    Technical2: '多语言支持，语言语音无缝切换',
+    Technical3: '超高速高质量规模化生产',
+  };
+  
