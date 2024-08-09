8d48e9ce184134ecb9bb06dffe40b2dfaf18f073
Yudi Xiao <shuohigh@gmail.com>
2024-08-09 16:52:21 +0800

    update package

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -1,5 +1,6 @@
 import '@/app/globals.css';
 import AntdStyledComponentsRegistry from '@/lib/antd-registry';
+import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
 import { Props } from '@/types/Layout';
 
@@ -7,11 +8,12 @@ import { Props } from '@/types/Layout';
 export default async function RootLayout({ children }: Partial<Props>) {
   return (
     <html lang="en">
-      <body>
-      <AntdStyledComponentsRegistry>
-        {children}
-      </AntdStyledComponentsRegistry>
-      </body>
+    <body>
+    <AntdStyledComponentsRegistry>
+      {children}
+    </AntdStyledComponentsRegistry>
+    <Analytics />
+    </body>
     </html>
   );
 }
