f8f99f14041a23c67806f1d5519d832f4b3bed20
Yudi Xiao <463708580@qq.com>
2024-08-13 10:32:26 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Props } from '@/types/Layout';
-import useSettingStore from '@/store/useSettingStore';
+// import useSettingStore from '@/store/useSettingStore';
 export const metadata: { title: string } = {
   title: '不忘智能科技',
 };
 
 export default function Layout({ children }: Props) {
-  const defaultLocale = useSettingStore((state) => state.defaultLocale);
+  // const defaultLocale = useSettingStore((state) => state.defaultLocale);
 
   return (
-    <html lang={defaultLocale}>
-      <body>
-        {children}
-      </body>
+    <html lang="en">
+    <body>
+    {children}
+    </body>
     </html>
   );
 }
